d9fe0e2f17d37bdbb1466a282d8895fa6b38b3f9
ddmail2009 <ddmail2009@gmail.com>
2013-12-26 19:38:48 +0800

    add gitignore

--- codeGen.c
+++ codeGen.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "codeGen.h"
 
 void Error(char *str){
@@ -6,4 +10,41 @@ void Error(char *str){
 
 void codeGen(AST_NODE* prog){
   Error("start codeGen");
+  genGeneralNode(prog);
+}
+
+void genGeneralNode(AST_Node *node){
+  switch(node->type()){
+    case PROGRAM_NODE:
+      genProgramNode(prog);
+      break;
+    case PROGRAM_NODE:
+      break;
+    case DECLARATION_NODE:
+      break;
+    case IDENTIFIER_NODE:
+      break;
+    case PARAM_LIST_NODE:
+      break;
+    case NUL_NODE:
+      break;
+    case BLOCK_NODE:
+      break;
+    case VARIABLE_DECL_LIST_NODE:
+      break;
+    case STMT_LIST_NODE:
+      break;
+    case STMT_NODE:
+      break;
+    case EXPR_NODE:
+      break;
+    case CONST_VALUE_NODE,:
+      break;
+    case NONEMPTY_ASSIGN_EXPR_LIST_NODE:
+      break;
+    case NONEMPTY_RELOP_EXPR_LIST_NOD:
+      break;
+    default:
+      Error(node, "Unknown Node type: %d", node->type())
+  }
 }
