69bff75940d29a3f323be46e43d59911216eb882
ddmail2009 <ddmail2009@gmail.com>
2014-01-03 22:30:52 +0800

    folder structure modified

---
+++ src/header.h
@@ -0,0 +1,305 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+
+#include <assert.h>
+#include <stdio.h>
+#define MAX_ARRAY_DIMENSION 10
+
+typedef enum DATA_TYPE{
+  INT_TYPE,
+  FLOAT_TYPE,
+  VOID_TYPE,
+  INT_PTR_TYPE,//for parameter passing
+  FLOAT_PTR_TYPE,//for parameter passing
+  CONST_STRING_TYPE,//for "const string"
+  NONE_TYPE,//for nodes like PROGRAM_NODE which has no type
+  ERROR_TYPE
+} DATA_TYPE;
+
+typedef enum IDENTIFIER_KIND{
+  NORMAL_ID, //function Name, uninitialized scalar variable
+  ARRAY_ID, //ID_NODE->child = dim
+  WITH_INIT_ID, //ID_NODE->child = initial value
+} IDENTIFIER_KIND;
+
+typedef enum BINARY_OPERATOR{
+  BINARY_OP_ADD,
+  BINARY_OP_SUB,
+  BINARY_OP_MUL,
+  BINARY_OP_DIV,
+  BINARY_OP_EQ,
+  BINARY_OP_GE,
+  BINARY_OP_LE,
+  BINARY_OP_NE,
+  BINARY_OP_GT,
+  BINARY_OP_LT,
+  BINARY_OP_AND,
+  BINARY_OP_OR
+} BINARY_OPERATOR;
+
+typedef enum UNARY_OPERATOR{
+  UNARY_OP_POSITIVE,
+  UNARY_OP_NEGATIVE,
+  UNARY_OP_LOGICAL_NEGATION
+} UNARY_OPERATOR;
+
+//C_type= type of constant ex: 1, 3.3, "const string"
+//do not modify, or lexer might break
+typedef enum C_type {
+  INTEGERC,
+  FLOATC,
+  STRINGC
+} C_type;
+
+typedef enum STMT_KIND{
+  WHILE_STMT,
+  FOR_STMT,
+  ASSIGN_STMT, //TODO:for simpler implementation, assign_expr also uses this
+  IF_STMT,
+  FUNCTION_CALL_STMT,
+  RETURN_STMT,
+} STMT_KIND;
+
+typedef enum EXPR_KIND{
+  BINARY_OPERATION,
+  UNARY_OPERATION
+} EXPR_KIND;
+
+typedef enum DECL_KIND{
+  VARIABLE_DECL,
+  TYPE_DECL,
+  FUNCTION_DECL,
+  FUNCTION_PARAMETER_DECL
+} DECL_KIND;
+
+typedef enum AST_TYPE{
+  PROGRAM_NODE,
+  DECLARATION_NODE,
+  IDENTIFIER_NODE,
+  PARAM_LIST_NODE,
+  NUL_NODE,
+  BLOCK_NODE,
+  VARIABLE_DECL_LIST_NODE,
+  STMT_LIST_NODE,
+  STMT_NODE,
+  EXPR_NODE,
+  CONST_VALUE_NODE, //ex:1, 2, "constant string"
+  NONEMPTY_ASSIGN_EXPR_LIST_NODE,
+  NONEMPTY_RELOP_EXPR_LIST_NODE
+} AST_TYPE;
+
+//*************************
+// AST_NODE's semantic value
+//*************************
+
+typedef struct STMTSemanticValue{
+  STMT_KIND kind;
+} STMTSemanticValue;
+
+typedef struct EXPRSemanticValue{
+  EXPR_KIND kind;
+
+  int isConstEval;
+
+  union{
+    int iValue;
+    float fValue;
+  } constEvalValue;
+
+  union{
+    BINARY_OPERATOR binaryOp;
+    UNARY_OPERATOR unaryOp;
+  } op;
+
+  BINARY_OPERATOR binaryOp() {
+    assert(kind == BINARY_OPERATION);
+    return op.binaryOp;
+  }
+
+  UNARY_OPERATOR unaryOp() {
+    assert(kind == UNARY_OPERATION);
+    return op.unaryOp;
+  }
+} EXPRSemanticValue;
+
+typedef struct DECLSemanticValue{
+  DECL_KIND kind;
+} DECLSemanticValue;
+
+typedef struct IdentifierSemanticValue{
+  char *identifierName;
+  struct SymbolTableEntry *symbolTableEntry;
+  IDENTIFIER_KIND kind;
+
+  const char* name() {
+    return identifierName;
+  }
+} IdentifierSemanticValue;
+
+typedef struct TypeSpecSemanticValue{
+  char *typeName;
+} TypeSpecSemanticValue;
+
+//don't modify or lexer may break
+typedef struct CON_Type{
+  C_type  const_type;
+  union {
+    int     intval;
+    double  fval;
+    char    *sc; 
+  } const_u;
+
+  C_type type() {
+    return const_type;
+  }
+  int Int() {
+    assert(type() == INTEGERC);
+    return const_u.intval;
+  }
+  double Double() {
+    assert(type() == FLOATC);
+    return const_u.fval;
+  }
+  char* Char() {
+    assert(type() == INTEGERC);
+    return const_u.sc;
+  }
+} CON_Type;
+
+/* add the part needed in this homework */
+enum RegisterType {
+    GENERAL,
+    FLOATING
+};
+
+
+// forward declaration
+// this method is in gen-part.cpp
+int getReg(RegisterType type);
+
+class AST_NODE {
+  public:
+    struct AST_NODE *child;
+    struct AST_NODE *parent;
+    struct AST_NODE *rightSibling;
+    struct AST_NODE *leftmostSibling;
+    AST_TYPE nodeType;
+    DATA_TYPE dataType;
+    int linenumber;
+
+    // store temporaries value in register no. ?
+    int place;
+    union {
+      IdentifierSemanticValue identifierSemanticValue;
+      STMTSemanticValue stmtSemanticValue;
+      DECLSemanticValue declSemanticValue;
+      EXPRSemanticValue exprSemanticValue;
+      CON_Type *const1;
+    } semantic_value;
+
+    void setSymbolTableEntry(SymbolTableEntry *entry){
+      assert(type() == IDENTIFIER_NODE);
+      semantic_value.identifierSemanticValue.symbolTableEntry = entry;
+    }
+
+    AST_TYPE type() {
+      return nodeType;
+    }
+
+    DATA_TYPE getDataType() {
+      return dataType;
+    }
+
+    void setDataType(DATA_TYPE type){
+      dataType = type;
+    }
+
+    STMT_KIND getStmtType(){
+      assert(type() == STMT_NODE);
+      return semantic_value.stmtSemanticValue.kind;
+    }
+
+    C_type getConType() {
+      assert(type() == CONST_VALUE_NODE);
+      return semantic_value.const1->type();
+    }
+
+    int getConIntValue() {
+      assert(getConType() == INTEGERC);
+      return semantic_value.const1->const_u.intval;
+    }
+
+    double getConFloatValue() {
+      assert(getConType() == FLOATC);
+      return semantic_value.const1->const_u.fval;
+    }
+
+    const char* getCharPtrValue() {
+      assert(getConType() == STRINGC);
+      return semantic_value.const1->const_u.sc;
+    }
+
+    DECL_KIND getDeclKind() {
+      assert(type() == DECLARATION_NODE);
+      return semantic_value.declSemanticValue.kind;
+    }
+
+    IDENTIFIER_KIND getIDKind() {
+      assert(type() == IDENTIFIER_NODE);
+      return semantic_value.identifierSemanticValue.kind;
+    }
+
+    EXPR_KIND getExprKind() {
+      assert(type() == EXPR_NODE);
+      return semantic_value.exprSemanticValue.kind;
+    }
+
+    BINARY_OPERATOR getBinaryOp() {
+      assert(type() == EXPR_NODE);
+      return semantic_value.exprSemanticValue.binaryOp();
+    }
+
+    UNARY_OPERATOR getUnaryOp() {
+      assert(type() == EXPR_NODE);
+      return semantic_value.exprSemanticValue.unaryOp();
+    }
+
+    const char* getIDName() {
+      assert(type() == IDENTIFIER_NODE);
+      return semantic_value.identifierSemanticValue.name();
+    }
+
+    SymbolTableEntry *getSymbol(){
+      assert(type() == IDENTIFIER_NODE);
+      return semantic_value.identifierSemanticValue.symbolTableEntry;
+    }
+
+    void setTemporaryPlace(){
+        if(getDataType() == INT_TYPE) {
+            place = getReg(GENERAL);
+        }
+        else if(getDataType() == FLOAT_TYPE) {
+            place = getReg(FLOATING);
+        }
+    }
+
+    void getTemporaryPlace(char *outStr){
+        if(getDataType() == FLOAT_TYPE)
+            sprintf(outStr, "f%d", place);
+        else 
+            sprintf(outStr, "%d", place);
+    }
+
+    char placeStr[1024];
+    const char *getTemporaryPlace(){
+        getTemporaryPlace(placeStr);
+        return placeStr;
+    }
+};
+
+struct SymbolTable;
+
+AST_NODE *Allocate(AST_TYPE type);
+void semanticAnalysis(AST_NODE *root);
+
+#endif
---
+++ src/symbolTable.h
@@ -0,0 +1,272 @@
+#ifndef __SYMBOL_TABLE_H__
+#define __SYMBOL_TABLE_H__
+// This file is for reference only, you are not required to follow the implementation. //
+
+#include "header.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+//SYMBOL_TABLE_PREINSERT_NAME
+#define SYMBOL_TABLE_INT_NAME "int"
+#define SYMBOL_TABLE_FLOAT_NAME "float"
+#define SYMBOL_TABLE_VOID_NAME "void"
+#define SYMBOL_TABLE_SYS_LIB_READ "read"
+#define SYMBOL_TABLE_SYS_LIB_FREAD "fread"
+#define HASH_TABLE_SIZE 256
+
+enum SymbolAttributeKind {
+  VARIABLE_ATTRIBUTE,
+  TYPE_ATTRIBUTE,
+  FUNCTION_SIGNATURE
+};
+
+enum TypeDescriptorKind {
+  SCALAR_TYPE_DESCRIPTOR,
+  ARRAY_TYPE_DESCRIPTOR,
+};
+
+class ArrayProperties{
+  public:
+    int dimension;
+    int sizeInEachDimension[MAX_ARRAY_DIMENSION];
+    DATA_TYPE elementType;
+
+    /* cannot use constructor because ArrayProperties is in union */
+    void init() {
+      dimension = 0;
+      for(int i = 0; i < MAX_ARRAY_DIMENSION; i++)
+        sizeInEachDimension[i] = -1;
+    }
+
+    bool isCompatible(const ArrayProperties p, bool ignoreFirstDimSize = true) const{
+      DATA_TYPE a = elementType, b = p.elementType;
+      if(!(a == b || (a == INT_TYPE && b == FLOAT_TYPE) || (a == FLOAT_TYPE && b == INT_TYPE)))
+        return false;
+      if(dimension != p.dimension) return false;
+
+      for(int i= ignoreFirstDimSize ? 1 : 0; i<dimension; i++)
+        if(sizeInEachDimension[i] != p.sizeInEachDimension[i]) return false;
+      return true;
+    }
+};
+
+class TypeDescriptor {
+  public:
+    TypeDescriptor(TypeDescriptorKind typeKind, DATA_TYPE type);
+    DATA_TYPE getDataType() const;
+    void addDimension(int sizeInDimension);
+    void addMissDimension();
+    int getDimension() const;
+    int getArrayDimensionSize(int idx) const;
+    bool getMissDimension() const;
+    bool isCompatible(const TypeDescriptor &b) const;
+    void getSubArrayProperty(int dimension, TypeDescriptor **temp) const;
+    TypeDescriptorKind getKind() const;
+ 
+
+    TypeDescriptorKind kind;
+    union {
+      DATA_TYPE dataType;//kind: SCALAR_TYPE_DESCRIPTOR
+      ArrayProperties arrayProperties;//kind: ARRAY_TYPE_DESCRIPTOR
+    } properties;
+
+    int size() const {
+        if(getKind() == SCALAR_TYPE_DESCRIPTOR) return 4;
+        else{
+            int size = 0;
+            for(int i=0; i<getDimension(); i++)
+                size += getArrayDimensionSize(i);
+            return (size*4+3)/4*4;
+        }
+    }
+};
+
+struct Parameter {
+  struct Parameter* next;
+  TypeDescriptor* type;
+  char* parameterName;
+
+  Parameter(const char *name, TypeDescriptor *type){
+    next = NULL;
+    this->type = type;
+    parameterName = new char[strlen(name)];
+    strcpy(parameterName, name);
+  }
+
+  const char *getParamName(){
+    return parameterName;
+  }
+};
+
+class FunctionSignature {
+  public:
+    FunctionSignature(DATA_TYPE type): returnType(type){
+      parametersCount = 0;
+      parameterList = NULL;
+    }
+
+    void addParameter(Parameter *p){
+      parametersCount += 1;
+      Parameter *iter = parameterList;
+
+      while(iter && iter->next != NULL) iter = iter->next;
+
+      if(iter) iter->next = p;
+      else parameterList = p;
+    }
+
+    DATA_TYPE getReturnType() const {
+      return returnType;
+    }
+
+    int getParameterCount() const{
+      return parametersCount;
+    }
+
+    Parameter * getParams() const{
+      return parameterList;
+    }
+
+    void print() const{
+      Parameter *iter = parameterList;
+      while(iter){
+        printf("Name[%s]type[%d], ", iter->parameterName, iter->type->getDataType());
+        iter = iter->next;
+      }
+      puts("");
+    }
+
+
+    int parametersCount;
+    Parameter *parameterList;
+    DATA_TYPE returnType;
+};
+
+class SymbolAttribute {
+  public:
+    SymbolAttribute(SymbolAttributeKind attrkind, TypeDescriptor* t) {
+      attributeKind = attrkind;
+      assert(attributeKind == VARIABLE_ATTRIBUTE || attributeKind == TYPE_ATTRIBUTE);
+      attr.typeDescriptor = t;
+    }
+
+    SymbolAttribute(SymbolAttributeKind attrkind, FunctionSignature *f) {
+      attributeKind = attrkind;
+      assert(attributeKind == FUNCTION_SIGNATURE);
+      attr.functionSignature = f;
+    }
+
+    TypeDescriptor *getTypeDes(){
+      assert(attributeKind == VARIABLE_ATTRIBUTE || attributeKind == TYPE_ATTRIBUTE);
+      return attr.typeDescriptor;
+    }
+
+    FunctionSignature* getFuncSig(){
+      assert(attributeKind == FUNCTION_SIGNATURE);
+      return attr.functionSignature;
+    }
+
+    SymbolAttributeKind getKind(){
+      return attributeKind;
+    }
+
+    DATA_TYPE getDataType(){
+      switch(attributeKind){
+        case VARIABLE_ATTRIBUTE: case TYPE_ATTRIBUTE:
+          return getTypeDes()->getDataType();
+        case FUNCTION_SIGNATURE:
+          return getFuncSig()->getReturnType();
+        default:
+          printf("Unknown attributeKind\n");
+      }
+      return NONE_TYPE;
+    }
+
+
+    SymbolAttributeKind attributeKind;
+    union {
+      TypeDescriptor* typeDescriptor;
+      FunctionSignature* functionSignature;
+    } attr;
+};
+
+class SymbolTableEntry {
+  public:
+    struct SymbolTableEntry* nextInHashChain;
+    struct SymbolTableEntry* prevInHashChain;
+    struct SymbolTableEntry* nextInSameLevel;
+    struct SymbolTableEntry* sameNameInOuterLevel;
+    char name[1024];
+    SymbolAttribute* attribute;
+    int nestingLevel;
+    // offset is for local variable allocation $fp + offset
+    int _offset;
+
+    SymbolTableEntry(int level, const char *n = NULL, SymbolAttribute *attr = NULL):
+        nextInHashChain(NULL),
+        prevInHashChain(NULL),
+        nextInSameLevel(NULL),
+        sameNameInOuterLevel(NULL),
+        attribute(attr),
+        nestingLevel(level),
+        _offset(0) {
+            assert(n != NULL);
+            strcpy(name, n);
+    }
+
+    // when this variable is local (in the precedure), set the offset in the
+    // stack
+    void setOffset(int o) { _offset = o; }
+    int offset() const { return _offset; }
+    void getLocation(AST_NODE *node, char *outStr){
+        int off = 0;
+        if(node->getIDKind() == ARRAY_ID) 
+            off = getArrayOffset(node);
+
+        if(nestingLevel == 0)
+            sprintf(outStr, "_%s", name);
+        else 
+            sprintf(outStr, "%d($fp)", offset() - off*4);
+    }
+
+private:
+    int getArrayOffset(AST_NODE *node){
+        int offset = 0;
+        if(node->getIDKind() == ARRAY_ID){
+            TypeDescriptor *type = node->getSymbol()->attribute->getTypeDes();
+            AST_NODE *dim = node->child;
+            int dimension = 0;
+            while(dim){
+                offset += dim->getConIntValue();
+                dim = dim->rightSibling;
+                if(!dim) break;
+
+                offset += type->getArrayDimensionSize(dimension);
+                dimension += 1;
+            }
+        }
+        return offset;
+    }
+};
+
+struct SymbolTable {
+  SymbolTableEntry* hashTable[HASH_TABLE_SIZE];
+  SymbolTableEntry** scopeDisplay;
+  int currentLevel;
+  int scopeDisplayElementCount;
+};
+
+
+void initializeSymbolTable();
+void symbolTableEnd();
+SymbolTableEntry* retrieveSymbol(char* symbolName);
+SymbolTableEntry* enterSymbol(char* symbolName, SymbolAttribute* attribute);
+void removeSymbol(char* symbolName);
+int declaredLocally(char* symbolName);
+void openScope();
+void closeScope();
+
+#endif
